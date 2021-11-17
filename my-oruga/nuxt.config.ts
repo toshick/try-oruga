c655a472d934705e121901c4a254959bfa1b0715
toshick <toshickjazz@mac.com>
2021-11-17 11:33:51 +0900

    feat: wip setup

---
+++ my-oruga/nuxt.config.ts
@@ -0,0 +1,7 @@
+import { defineNuxtConfig } from 'nuxt3';
+
+// https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
+export default defineNuxtConfig({
+  modules: ['@oruga-ui/oruga/nuxt'],
+  // plugins: [{ src: '~plugins/oruga.js' }],
+});
---
+++ my-oruga/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <main>いんでっくす</main>
+</template>
+<script setup lang="ts">
+console.log("セットアップ");
+import { createApp } from "vue";
+import Oruga from "@oruga-ui/oruga-next";
+import "@oruga-ui/oruga-next/dist/oruga.css";
+
+createApp({}).use(Oruga);
+</script>
